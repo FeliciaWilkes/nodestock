b6c5a1fb110d51ba19673ebca2117cd061d3b8c3
Felicia Wilkes <feliciankc@gmail.com>
2021-01-14 09:51:45 -0600

    Build Webpage

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
-    //use hosting port and settings or use 5000;
+const express = require('express');
+const app = express();
+const path = require('path');
+//use hosting port and settings or use 5000;
 const PORT = process.env.PORT || 5000;
 
-
+// Set static folder (can be named anything (public ex.))
+//Serve static files from multiple directories, but give precedence to “./public” over the others:
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
